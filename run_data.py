cba8c9a33c41a85ab737c3190a252c276fea19ba
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-01-10 16:50:38 -0300

    Fix get data by paramaters and add pretty print

--- run_data.py
+++ run_data.py
@@ -1,8 +1,16 @@
-import sys, json
+from sys import argv
+from json import loads, dumps
 
 def print_message(msg):
-    return {"inbound_message": msg}
+	return {"inbound_message": msg}
 
 if __name__ == '__main__':
-    request = json.load( sys.stdin )
-    print(request)
+	# print('Number of arguments:', len(argv), 'arguments.')
+	# print('Argument List:', str(argv))
+	# print(dumps(print_message(argv[2]), indent=4, sort_keys=True))
+    data = loads(argv[1])
+    print(dumps(print_message(data), indent=4, sort_keys=True))
+
+
+
+
