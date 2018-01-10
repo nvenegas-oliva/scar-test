5c76d96eede8fc7e61eb010a992201dbca301af1
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-01-10 17:10:07 -0300

    change agrv parameter

--- run_data.py
+++ run_data.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 	# print('Number of arguments:', len(argv), 'arguments.')
 	# print('Argument List:', str(argv))
 	# print(dumps(print_message(argv[2]), indent=4, sort_keys=True))
-    data = loads(argv[1])
+    data = loads(argv[2])
     print(dumps(print_message(data), indent=4, sort_keys=True))
 
 
