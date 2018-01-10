f3bb9c5842d91037a9534e01bff6b17179118b37
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-01-10 16:18:14 -0300

    print json request

--- run_data.py
+++ run_data.py
@@ -1,9 +1,8 @@
-from sys import argv
-
+import sys, json
 
 def print_message(msg):
-	return {"inbound_message": msg}
+    return {"inbound_message": msg}
 
 if __name__ == '__main__':
-	print('Number of arguments:', len(argv), 'arguments.')
-	print('Argument List:', str(argv))
+    request = json.load( sys.stdin )
+    print(request)
