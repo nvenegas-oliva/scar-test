a62cae31bf3a0d8e3ecb5da6b67303653a3f4065
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-01-10 17:30:39 -0300

    Update

--- run_data.py
+++ run_data.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
 	# print('Number of arguments:', len(argv), 'arguments.')
 	# print('Argument List:', str(argv))
 	# print(dumps(print_message(argv[2]), indent=4, sort_keys=True))
-    data = loads(str(argv))
-    print(dumps(print_message(data), indent=4, sort_keys=True))
+    data = str(argv)
+    #print(dumps(print_message(data), indent=4, sort_keys=True))
+    print(data)
 
 
 
